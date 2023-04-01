e2aa110ebdd41f2965a2f9c93b72b9f158664142
AlexanderPenny <2200336@uad.ac.uk>
2023-04-01 16:00:32 +0100

    Start of a Health Bar
    Not near done tbh

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{
+    void resetHealth()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
