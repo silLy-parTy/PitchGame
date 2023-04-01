89eb19b0f9987cf4dcd45c3c5623431c87d1bb35
Forg28 <2200768@uad.ac.uk>
2023-04-01 16:39:19 +0100

    Camera Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z -10);
     }
 }
