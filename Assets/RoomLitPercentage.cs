adccb0ed908aebba63113fbc27ad9942af5af185
AlexanderPenny <2200336@uad.ac.uk>
2023-04-01 19:46:21 +0100

    Percentage Completion added

---
+++ Assets/RoomLitPercentage.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomLitPercentage : MonoBehaviour
+{
+    public Text textbox;
+
+    private void Start()
+    {
+        textbox.text = "0%";
+    }
+
+    void setText(string newText)
+    {
+        textbox.text = newText;
+    }
+}
