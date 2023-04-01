1399681b0ae4cac88f003f91a83b85043887f0b8
Forg28 <2200768@uad.ac.uk>
2023-04-01 16:41:29 +0100

    Revert "Generic Health Bar Completed"
    This reverts commit 03352d932e1c21285c59ad9bed88c6592f4178d8.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBar : MonoBehaviour
+public class playerHealth : MonoBehaviour
 {
+    void resetHealth()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        resetHealth();
     }
 
     // Update is called once per frame
