03352d932e1c21285c59ad9bed88c6592f4178d8
AlexanderPenny <2200336@uad.ac.uk>
2023-04-01 16:40:30 +0100

    Generic Health Bar Completed
    Not specific to player or other items

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerHealth : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
-    void resetHealth()
-    {
-
-    }
     // Start is called before the first frame update
     void Start()
     {
-        resetHealth();
+        
     }
 
     // Update is called once per frame
