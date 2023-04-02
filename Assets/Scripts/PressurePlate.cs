0f67b5f7ecad13c97275d247c3d2ae1096af358d
Forg28 <2200768@uad.ac.uk>
2023-04-02 15:29:30 +0100

    Fixed the door and pressure plate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -7,6 +7,7 @@ public class PressurePlate : MonoBehaviour
     [SerializeField] private GameObject doorGameObject;
     private Door door;
 
+    public Animator anim;
     public GameObject presPlate;
 
     private void Awake()
