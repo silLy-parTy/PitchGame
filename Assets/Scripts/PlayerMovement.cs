7868bda3710470974b3b42366d31c71397359dec
Forg28 <2200768@uad.ac.uk>
2023-04-02 11:26:25 +0100

    Adding Tilemap, animations etc.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,10 @@ public class PlayerMovement : MonoBehaviour
     //Declare Variables
     public float moveSpeed = 1f;
 
+    public bool facingRight = true;
+
     public Rigidbody2D rb;
+    public Animator anim;
 
     Vector2 movement;
 
@@ -16,10 +19,35 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(movement.y);
+
+        anim.SetFloat("SpeedX", Mathf.Abs(movement.x));
+
+        if (movement.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+
+        if (movement.x < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed);
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
